218639427757983c5ad793905f9719ce8ab6674b
littlekite <15829705867@sina.cn>
2023-03-02 15:16:51 +0800

    3

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,29 @@ fn main() {
         },
         ..default()
     }));
+
+    //RPG 颜色转化%255
+    app.insert_resource(ClearColor(Color::rgb(1., 1., 0.87)));
+
     app.add_startup_system(step);
     app.run();
 }
 
-fn step(){
-    println!("hello world")
+fn step(
+    mut commands:Commands
+){
+    //生产照相机
+    // Spawn the camera
+    let camera_bundle =  Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 1000.),
+        projection: OrthographicProjection {
+            scale: 0.5,
+            ..default()
+        },
+        ..default()
+    };
+    // camera_bundle.orthographic_projection.depth_calculation = DepthCalculation::Distance;;
+    commands.spawn(camera_bundle);
+
+
 }
