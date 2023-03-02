b05d303cfd7e91f593fcfa6110f9607f6e60de0b
littlekite <15829705867@sina.cn>
2023-03-02 11:46:50 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ use bevy::prelude::*;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            width: 800.,
+            height: 600.,
+            title: "Bevy game".to_string(), // ToDo
+            canvas: Some("#bevy".to_owned()),
+            ..Default::default()
+        },
+        ..default()
+    }));
     app.add_startup_system(step);
     app.run();
 }
