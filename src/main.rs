3bcde6a06489a320829a2d4fd92670aca2f6bd3b
littlekite <15829705867@sina.cn>
2023-03-02 10:22:26 +0800

    123

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("hello world");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_startup_system(step);
+    app.run();
+}
+
+fn step(){
+    println!("hello world")
 }
